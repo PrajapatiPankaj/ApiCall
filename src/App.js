36010b94678549def16e5a373a68161d15c15fd4
PankajPrajapati <paprajapati.25.1@gmail.com>
2023-02-27 18:28:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect,useState} from "react";
+import "./App.css";
 
 function App() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+const [data , setData] = useState([])
+const [email,setEmail]=useState();
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json)=>{
+              console.log(json);
+              setData(json);
+          })
+      .catch((e) => console.log("e", e));
+  }, []);
+
+  const AddPost = ()=>{
+
+     fetch(url, {
+       method: "POST", // or 'PUT'
+       headers: {
+         "Content-Type": "application/json",
+       },
+       body: JSON.stringify(email),
+     })
+       .then((response) => response.json())
+       .then((data) => {
+         console.log("Success:", email);
+       })
+       .catch((error) => {
+         console.error("Error:", error);
+       });
+
+      setEmail("")
+  }
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        Welcome
+        {console.log("Data", data)}
+        {data.map((item) => (
+          <ul key={item.id}>
+            <li>{item.email}</li>
+          </ul>
+        ))}
+      </div>
+
+      <div>
+          <input type="text" name="email" onChange={(e)=>setEmail(e.target.value)} placeholder="Email"/>
+          {console.log("Email", email)}
+          <button onClick={AddPost}>Submit</button>
+      </div>
+    </>
   );
 }
 
